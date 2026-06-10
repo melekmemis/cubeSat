cf55309448ea368db2340c5df4b39f591642a31d
Melek Memiş <memismelek@yahoo.com>
2026-06-10 15:24:57 +0300

    first commit

--- csCenter/Core/Src/sdCard.c
+++ csCenter/Core/Src/sdCard.c
@@ -36,8 +36,8 @@ void sdCardWrite() {
 	} else {
 	}
 	char str[200];
-	sprintf(str, "Time: %d:%d:%d, accX: %f, accY: %f, accZ: %f, pitch: %f, roll: %f, yaw: %f, Altitude: %f\r\n",
-		    	             sensorData.rtcHour, sensorData.rtcMinute, sensorData.rtcSecond, sensorData.accX, sensorData.accY, sensorData.accZ, sensorData.pitch, sensorData.roll, sensorData.yaw, sensorData.altitude);
+	sprintf(str, "Time: %d:%d:%d, accX: %f, accY: %f, accZ: %f, pitch: %f, roll: %f, yaw: %f, Altitude: %f, SensorC BatV: %f, SensorC BatC: %f\r\n",
+		    	             sensorData.rtcHour, sensorData.rtcMinute, sensorData.rtcSecond, sensorData.accX, sensorData.accY, sensorData.accZ, sensorData.pitch, sensorData.roll, sensorData.yaw, sensorData.altitude, sensorData.busVoltageFloat, sensorData.currentFloat);
 
 	dataSize = (int)strlen(str);
 
